e94394efe5d71d75c5c5e1cf6a1e97c6eb2fd5b7
EliSH-Gi <elaheh.shabanian@gmail.com>
2024-09-24 16:00:26 +0200

    create basic Structure of Project.

---
+++ README.mk
@@ -0,0 +1,16 @@
+
+
+git init
+git add nameFile.e
+git commit -m "add nameFile"
+**git log => 
+Author: EliSH-Gi <elaheh.shabanian@gmail.com>
+Date:   Tue Sep 24 14:41:23 2024 +0200
+
+    linked style.css to index.html
+	commit 4b3ca48229f06a1fccd098a47a11a9929d9aedb2
+	**
+git log -- oneline
+**4b3ca48 (HEAD -> main) linked style.css to index.html**
+git remote add origin https://github.com/your_username/your_repo_name.git
+git push -u origin main
